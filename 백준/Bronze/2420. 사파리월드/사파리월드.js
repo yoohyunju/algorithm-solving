5ad4700d66197501715517ebfef91262b061684c
string_main <59650985+yoohyunju@users.noreply.github.com>
2022-07-07 23:48:15 +0900

    Update 사파리월드.js

--- 백준/Bronze/2420. 사파리월드/사파리월드.js
+++ 백준/Bronze/2420. 사파리월드/사파리월드.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
 
-const A = parseInt(input[0]) // 왜 BigInt여야 통과될까
+const A = parseInt(input[0])
 const B = parseInt(input[1])
 
 console.log(Math.abs(A-B))
