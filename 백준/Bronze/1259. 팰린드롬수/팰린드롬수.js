1b87ff29053aab419065e8a3208d629fdbfe2449
string_main <59650985+yoohyunju@users.noreply.github.com>
2022-08-15 17:32:05 +0900

    [Bronze I] Title: 팰린드롬수, Time: 124 ms, Memory: 9436 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split('\n');
+
+input.pop();
+
+for (let i = 0; i < input.length; i++) {
+	let reverseStr = input[i].split("").reverse().join("");
+	console.log(input[i] === reverseStr ? "yes" : "no");
+}
