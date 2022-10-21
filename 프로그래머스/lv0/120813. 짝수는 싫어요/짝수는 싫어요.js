d412fe9e3e60fe2800b0802a1191edae6cd68e59
string_main <59650985+yoohyunju@users.noreply.github.com>
2022-10-21 23:46:05 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let answer = [];
+    
+    for(let i = 0; i <= n; i++){
+        if(i % 2 === 1){
+            answer.push(i);
+        }
+    }
+    
+    return answer;
+}
