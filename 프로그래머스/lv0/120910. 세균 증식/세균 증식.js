1df8b8b8c7119101cd32c729a59adeb97f422ebd
string_main <59650985+yoohyunju@users.noreply.github.com>
2022-12-04 16:22:40 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.js
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.js
@@ -1,11 +1,3 @@
 function solution(n, t) {
-    //     let answer = n;
-
-    //     for(let i = 0; i < t; i++) {
-    //         answer *= 2;
-    //     }
-
-    //     return answer;
-    
     return (t === 0) ?  n : solution(n, t - 1) * 2;
 }
