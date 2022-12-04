226cb9a5c939df8adc24157f99fde9d2b2cb2ece
string_main <59650985+yoohyunju@users.noreply.github.com>
2022-12-04 16:21:38 +0900

    Update 세균 증식.js

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.js
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.js
@@ -1,9 +1,11 @@
 function solution(n, t) {
-    let answer = n;
+    //     let answer = n;
+
+    //     for(let i = 0; i < t; i++) {
+    //         answer *= 2;
+    //     }
+
+    //     return answer;
     
-    for(let i = 0; i < t; i++) {
-        answer *= 2;
-    }
-    
-    return answer;
+    return (t === 0) ?  n : solution(n, t - 1) * 2;
 }
