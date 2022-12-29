9eba68a589e9e12f905ec638cd563eefc84322af
string_main <59650985+yoohyunju@users.noreply.github.com>
2022-12-29 23:50:17 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,14 +1,8 @@
 function solution(s) {
-    let obj = {};
-    let answer = '';
-    
-    for(let i of s) {
-        obj[i] = (obj[i] || 0) + 1;
+    let arr = [];
+    for (let i of s) {
+        if (s.indexOf(i) === s.lastIndexOf(i)) 
+            arr.push(i);
     }
-    
-    for(let i in obj) {
-        if(obj[i] === 1) answer += i;
-    }
-    
-    return answer.split('').sort().join('');
+    return arr.sort().join('');
 }
