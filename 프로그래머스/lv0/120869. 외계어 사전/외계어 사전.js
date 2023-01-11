8ac97b0b30bbb1fa714f5355256f5ee4635f1a6c
string_main <59650985+yoohyunju@users.noreply.github.com>
2023-01-11 13:53:45 +0900

    [level 0] Title: 외계어 사전, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -1,19 +1,3 @@
 function solution(spell, dic) {
-    let flag = true;
-    
-    for(let i = 0; i < dic.length; i++) {
-        let arr = dic[i].split('');
-        flag = true;
-        
-        for(let j = 0; j < spell.length; j++) {
-            if(!arr.includes(spell[j])) {
-                flag = false;
-                break;
-            }
-        }
-        
-        if(flag) return 1;
-    }
-    
-    return 2;
+    return dic.filter(v => spell.every(c => v.includes(c))).length ? 1 : 2;
 }
