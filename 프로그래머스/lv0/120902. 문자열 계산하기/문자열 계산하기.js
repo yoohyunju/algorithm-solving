9979d1ce1893d75faed36846cc1147806275862e
string_main <59650985+yoohyunju@users.noreply.github.com>
2023-01-13 09:35:29 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -0,0 +1,14 @@
+function solution(my_string) {
+    const arr = my_string.split(' ');
+    let result = +arr[0];
+    
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] === "+") {
+            result += +arr[i + 1];
+        } else if(arr[i] === "-") {
+            result -= +arr[i + 1];
+        }
+    }
+    
+    return result;
+}
