11df213d8fc2589fb1914c0e6d674dce147e6a4b
string_main <59650985+yoohyunju@users.noreply.github.com>
2023-01-15 14:35:53 +0900

    [level 0] Title: OX퀴즈, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
@@ -1,19 +1,9 @@
 function solution(quiz) {
-    let answer = [];
-    
-    for(let i of quiz) {
-        let expression = i.split(' ');
-        let operand1 = Number(expression[0]);
-        let operand2 = Number(expression[2]);
-        let result = Number(expression[4]);
-        
-        if(i.includes("+")) {
-            operand1 + operand2 === result ? answer.push("O") : answer.push("X");
-        }else {
-            operand1 - operand2 === result ? answer.push("O") : answer.push("X");
-        }
-        
-    }
-    
-    return answer;
+    return quiz.map(v => {
+        const [expression, result] = v.split(' = ');
+        const operator = expression.includes('+') ? 1 : -1;
+        const [operand1, operand2] = expression.split(operator === 1 ? ' + ' : ' - ');
+
+        return +operand1 + (+operand2 * operator) === +result ? 'O' : 'X';
+    });
 }
