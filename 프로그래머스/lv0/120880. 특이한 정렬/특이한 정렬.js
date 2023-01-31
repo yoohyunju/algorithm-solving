f732e06fbd74f600deb84c77ae51e03f084c469a
string_main <59650985+yoohyunju@users.noreply.github.com>
2023-02-01 00:29:48 +0900

    [level 0] Title: 특이한 정렬, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,7 +1,3 @@
 function solution(numlist, n) {
-    return numlist.sort((a, b) => {
-        const [aGab, bGab] = [Math.abs(a - n), Math.abs(b - n)];
-        if(aGab === bGab) return b-a;
-        return aGab-bGab;
-    })
+    return numlist.sort((a, b) => Math.abs(a - n) - Math.abs(b - n) || b - a);
 }
