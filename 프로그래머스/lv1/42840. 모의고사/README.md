2756585052d5be0002169d7f9071e603f18a96b1
string_main <59650985+yoohyunju@users.noreply.github.com>
2023-03-13 04:21:09 +0100

    [level 1] Title: 모의고사, Time: 1.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/README.md
+++ 프로그래머스/lv1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 36.8 MB, 시간: 3.72 ms
+메모리: 33.6 MB, 시간: 1.11 ms
 
 ### 구분
 
--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -1,29 +1,22 @@
 function solution(answers) {
     let answer = [];
-    let max = 0;
-    let arr = [0, 0, 0];
+    let [a, b, c] = [0, 0, 0];
     const 수포자1 = [1, 2, 3, 4, 5];
     const 수포자2 = [2, 1, 2, 3, 2, 4, 2, 5];
     const 수포자3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
     
     
     for(let i = 0; i < answers.length; i++) {
-        if(answers[i] === 수포자1[i % 수포자1.length]) arr[0]++;
-        if(answers[i] === 수포자2[i % 수포자2.length]) arr[1]++;
-        if(answers[i] === 수포자3[i % 수포자3.length]) arr[2]++;
+        if(answers[i] === 수포자1[i % 수포자1.length]) a++;
+        if(answers[i] === 수포자2[i % 수포자2.length]) b++;
+        if(answers[i] === 수포자3[i % 수포자3.length]) c++;
     }
 
-    max = arr[0];
-    answer.push(1);
-    
-    for(let i = 1; i < arr.length; i++) {
-        if(max < arr[i]) {
-            answer[0] = i + 1;
-            max = arr[i];
-        } else if(max === arr[i]) {
-            answer.push(i + 1);
-        }
-    }
+    let max = Math.max(a, b, c);
+
+    if (a === max) answer.push(1);
+    if (b === max) answer.push(2);
+    if (c === max) answer.push(3);
     
     return answer;
 }
