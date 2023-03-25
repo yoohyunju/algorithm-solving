08dbaf26fd31d0cdf28f800b8995eb5d5038b1f3
string_main <59650985+yoohyunju@users.noreply.github.com>
2023-03-25 21:47:13 +0900

    [level 1] Title: 실패율, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,24 +1,13 @@
 function solution(N, stages) {
-    const answer = [];
-    const data = [];
-    const users = new Array(N + 1).fill(0);
+    let result = [];
     
-    // stage별 사용자 count
-    for(let i = 0; i < stages.length; i++) {
-        users[stages[i] - 1] ++;
+    for(let i = 1; i <= N; i++){
+        let reach = stages.filter((x) => x >= i).length;
+        let curr = stages.filter((x) => x === i).length;
+        result.push([i, curr / reach]);
     }
     
-    // 스테이지, 실패율 객체를 data 배열에 추가
-    for(let i = 0; i < N; i++) {
-        let range = users.slice(i);
-        let failureRate = users[i] / range.reduce((acc, cur) => acc + cur);
-        
-        data.push({stage: i+1, rate: failureRate});
-    }
+    result.sort((a, b) => b[1] - a[1]);
     
-    // 실패율 기준으로 내림차순 정렬 후 answer에 스테이지 번호 추가
-    data.sort((a, b) => b.rate - a.rate);
-    data.map(e => answer.push(e.stage));
-
-    return answer;
+    return result.map((x) => x[0]);
 }
