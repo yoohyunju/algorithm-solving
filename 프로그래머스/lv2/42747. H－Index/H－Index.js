ea9928429752bccbf642a2f1bcaab9f1bd66bd89
string_main <59650985+yoohyunju@users.noreply.github.com>
2023-03-29 23:58:35 +0900

    [level 2] Title: H-Index, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -0,0 +1,13 @@
+function solution(citations) {
+    var answer = 0;
+    
+    citations.sort((a, b) => b - a);
+
+    for(let i = 0; i < citations.length; i++) {
+        if(i < citations[i]) {
+            answer++;
+        }
+    }
+    
+    return answer;
+}
