768c9ab60934f3276a9dcee804d7688c992b0e8a
string_main <59650985+yoohyunju@users.noreply.github.com>
2024-07-03 00:25:24 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,7 +1,4 @@
 function solution(num_list) {
-    const [beforeEl, lastEl] = num_list.slice(-2)
-    const newEl = lastEl > beforeEl ? lastEl - beforeEl : lastEl * 2
-    num_list.push(newEl)
-    
-    return num_list
+    const [beforeNum, lastNum] = num_list.slice(-2)
+    return [...num_list, lastNum > beforeNum ? lastNum - beforeNum : lastNum * 2]
 }
