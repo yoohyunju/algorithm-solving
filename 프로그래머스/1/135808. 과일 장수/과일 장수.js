309f20291968b8f80eaab04a8ea5109277135720
string_main <59650985+yoohyunju@users.noreply.github.com>
2025-01-13 22:13:09 +0900

    [level 1] Title: 과일 장수, Time: 340.53 ms, Memory: 96.7 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -2,7 +2,6 @@ function solution(k, m, score) {
     let answer = 0
     let box = []
     const sortedScore = score.sort((a, b) => b - a)
-    const boxCount = score.length % m
     
     sortedScore.forEach(i => {   
         box.push(i)
